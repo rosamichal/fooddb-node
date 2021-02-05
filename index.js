eb17b4b2b99124a134849a262930882e7344a63b
Michał Rosa <michal.dew@gmail.com>
2021-02-05 21:47:58 +0100

    Added connection to database

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ const express = require('express');
 const app = express();
 const{ port } = require('./config');
 
+// db
+require('./db/mongoose');
+
 // server
 app.listen(port, () => {console.log(`Server is running... http://localhost:${port}`);});
